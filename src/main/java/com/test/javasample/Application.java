4e0d8f2e2f47518def1f2969642f06bd8dfb1218
song <738139341@qq.com>
2021-04-08 10:01:29 +0800

    init project

---
+++ src/main/java/com/test/javasample/Application.java
@@ -0,0 +1,11 @@
+package com.test.javasample;
+
+public class Application {
+    public static void main(String[] args) {
+        new Application().hello();
+    }
+
+    public void hello(){
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/test/javasample/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.test.javasample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void hello() {
+        new Application().hello();
+    }
+}
